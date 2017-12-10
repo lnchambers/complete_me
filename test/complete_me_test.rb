3e885448fc1683438dd6a9124d28886696c49a84
slimecog <wlcjohnson@gmail.com>
2017-12-09 19:45:39 -0700

    populate functional

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -17,12 +17,58 @@ class CompleteMeTest < Minitest::Test
     assert_equal 0, complete_me.word_count
   end
 
-  def test_that_insert_creates_new_node
+  def test_word_downcase_downcases_given_word
     complete_me = CompleteMe.new
-    complete_me.insert("a")
 
-    # assert_equal "a", complete_me.root.children.key
-    assert_instance_of Node, complete_me.root.children.values
+    assert_equal "word", complete_me.downcase_word("WoRd")
   end
 
+  def test_create_array_of_nodes_creates_an_array_of_node_instances
+    complete_me = CompleteMe.new
+
+    downcased_word = complete_me.downcase_word("word")
+
+    assert_instance_of Array, complete_me.create_array_of_nodes(downcased_word)
+    assert_equal "w", complete_me.create_array_of_nodes(downcased_word).first.letter
+    assert_equal "d", complete_me.create_array_of_nodes(downcased_word).last.letter
+  end
+
+  # def test_full_format_creates_array_of_node_instances
+  #   complete_me = CompleteMe.new
+  #
+  #   assert_equal
+  # end
+
+  def test_that_insert_creates_new_node
+    complete_me = CompleteMe.new
+    complete_me.insert("abc")
+
+    assert_equal "a", complete_me.root.children.first.first
+    assert_instance_of Array, complete_me.root.children.values
+  end
+
+  def test_that_word_count_increases_after_insert
+    complete_me = CompleteMe.new
+    complete_me.insert("abc")
+
+    assert_equal 1, complete_me.word_count
+
+    complete_me.insert("cba")
+
+    assert_equal 2, complete_me.word_count
+
+    complete_me.insert("three")
+    complete_me.insert("four")
+    complete_me.insert("five")
+
+    assert_equal 5, complete_me.word_count
+  end
+
+  def test_populate_add_words
+    complete_me = CompleteMe.new
+    complete_me.populate("mock_dict.txt")
+    assert_equal 10, complete_me.count
+    complete_me.insert("whatever")
+    assert_equal 11, complete_me.word_count
+  end
 end
