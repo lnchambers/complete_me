1ac48bb15c2d5b4d690ace121eb9d1c83a4514e3
Luke Chambers <lukenatchambers@gmail.com>
2017-12-12 15:06:07 -0700

    Add dependencies to gemfile

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -34,5 +34,65 @@ class CompleteMeTest < Minitest::Test
     assert_equal "w", complete_me.trie.root.children.first[1].children.first[1].children.first[0]
   end
 
+  def test_suggest_returns_an_array_of_words
+    complete_me = CompleteMe.new
 
+    complete_me.insert("yourself")
+    complete_me.insert("you")
+    complete_me.insert("yours")
+    complete_me.insert("your")
+
+    assert_instance_of Array, complete_me.suggest("you")
+    assert_equal "you", complete_me.suggest("you").first
+  end
+
+  def test_select_returns_selected_array_of_words
+    complete_me = CompleteMe.new
+
+    complete_me.insert("yourself")
+    complete_me.insert("you")
+    complete_me.insert("yours")
+    complete_me.insert("your")
+
+    assert_instance_of Array, complete_me.suggest("you")
+    assert_equal "you", complete_me.suggest("you").first
+
+    complete_me.select("you", "yours")
+
+    assert_instance_of Array, complete_me.suggest("you")
+    assert_equal "yours", complete_me.suggest("you").first
+
+    complete_me.select("you", "your")
+    complete_me.select("you", "your")
+
+    assert_equal "your", complete_me.suggest("you").first
+  end
+
+  def test_weight_adder_for_added_weight
+    complete_me = CompleteMe.new
+
+    complete_me.insert("yourself")
+    complete_me.insert("you")
+    complete_me.insert("yours")
+    complete_me.insert("your")
+
+    complete_me.select("you", "yours")
+    complete_me.select("you", "yours")
+
+    assert_equal 3, complete_me.weight_adder("you", "yours")
+  end
+
+  def test_weight_sorter_to_return_highest_weighted_word_first_in_array
+    complete_me = CompleteMe.new
+
+    complete_me.insert("yourself")
+    complete_me.insert("you")
+    complete_me.insert("yours")
+    complete_me.insert("your")
+
+    complete_me.select("you", "yours")
+    complete_me.select("you", "yours")
+
+    assert_equal ["yours"], complete_me.weight_sorter("you")
+  end
 end
