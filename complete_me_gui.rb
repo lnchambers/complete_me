7728d93348187e2adebc7e256cf3561ed4b92af8
ruby_road_to_ruin <zac.palmquist@gmail.com>
2017-12-12 15:12:41 -0700

    More shoes stuff

--- complete_me_gui.rb
+++ complete_me_gui.rb
@@ -8,6 +8,11 @@ cm.populate_from_csv_file(addresses)
 
 Shoes.app(title: "CompleteMe", width: 550) do
   background "#99ffcc".."#00b359"
+  @complete_me_png = image(
+      "http://www.sandboxthreads.com/media/catalog/product/cache/1/image/510x/602f0fa2c1f0d1ba5e241f914e856ff9/g/a/galaga_3.png",
+      top:  20,
+      right: 20
+    )
   flow(margin: [15, 15, 15, 5]) do
     para("Enter the start of a word to see suggestions.")
   end
